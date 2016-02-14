9406d3590e6c885d831602386fc454311fa0f9c1
Cameron Seebach <pannewb@gmail.com>
2016-02-14 13:20:46 -0600

    remove roslaunch lines from setup shell files

--- utilities/pc/pc_setup.sh
+++ utilities/pc/pc_setup.sh
@@ -1,11 +1,9 @@
 source ../venv/bin/activate    #activate the virtualenv
 
 #download the ip address to thewrong.info
-python pc_setup.py  
+python pc_setup.py
 
-deactivate #deactivate the virtualenv    
+deactivate #deactivate the virtualenv
 
 #set the environment for the master node to run
 source pc_ip.sh #contains export for ROS_HOSTNAME and ROS_MASTER_URI
-
-rqt -s kobuki_dashboard
--- utilities/turtle/turtle_setup.sh
+++ utilities/turtle/turtle_setup.sh
@@ -1,14 +1,12 @@
 source ../venv/bin/activate    #activate the virtualenv
 
 #upload the ip address to thewrong.info
-python turtle_setup.py  
+python turtle_setup.py
 
-deactivate #deactivate the virtualenv    
+deactivate #deactivate the virtualenv
 
 #set the environment for the master node to run
 export ROS_MASTER_URI=http://localhost:11311/
 source turtle_ip.sh #contains export for ROS_HOSTNAME
 
 export TURTLEBOT_3D_SENSOR=kinect
-
-roslaunch turtlebot_bringup minimal.launch --screen
